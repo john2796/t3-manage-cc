c6078cdc83d093e3d4c1ea700126fc181a2f99ca
john2796 <jbmiranda22796@gmail.com>
2023-06-20 06:15:11 -0700

    sidebard style mantine

--- src/components/shell/_main-links.tsx
+++ src/components/shell/_main-links.tsx
@@ -1,17 +1,18 @@
+import { Group, ThemeIcon, UnstyledButton, Text } from "@mantine/core";
+import { IconDashboard, IconPencil } from "@tabler/icons-react";
 import Link from "next/link";
 import React from "react";
 
 const links = [
   {
-    icon: 
-    label: "Landing Page",
+    icon: <IconDashboard size="1rem" />,
+    color: "red",
+    label: "Dashboard",
     href: "/",
   },
   {
-    label: "Dashboard",
-    href: "/dashboard",
-  },
-  {
+    icon: <IconPencil size="1rem" />,
+    color: "blue",
     label: "Manage Course",
     href: "/dashboard/courses",
   },
@@ -21,9 +22,32 @@ export function MainLinks() {
   return (
     <>
       {links.map((link) => (
-        <Link key={link.label} href={link.href}>
-          {link.label}
-        </Link>
+        <UnstyledButton
+          component={Link}
+          href={link.href}
+          key={link.href}
+          sx={(theme) => ({
+            display: "block",
+            width: "100%",
+            padding: theme.spacing.xs,
+            borderRadius: theme.radius.sm,
+            color:
+              theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+            "&:hover": {
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[0],
+            },
+          })}
+        >
+          <Group>
+            <ThemeIcon color={link.color} variant="light">
+              {link.icon}
+            </ThemeIcon>
+            <Text size="sm">{link.label}</Text>
+          </Group>
+        </UnstyledButton>
       ))}
     </>
   );
